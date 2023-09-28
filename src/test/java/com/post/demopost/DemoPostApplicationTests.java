2a825e330a72815660e51e55a901a5016d0f3a19
ErickBrayan <ebrayan2000@gmail.com>
2023-09-27 23:31:03 -0500

    fisrt commit

---
+++ src/test/java/com/post/demopost/DemoPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.demopost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
